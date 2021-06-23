5eae0e846ab7454f630d0710c46df2336df3b0c6
Ofek Kaveh <kaofek@gmail.com>
2021-06-23 16:45:57 +0300

    debug and design fix

--- Gui_3D_Bio.py
+++ Gui_3D_Bio.py
@@ -20,6 +20,12 @@ class Gui_3D_Bio:
         self.__firstCheckBoxStatus = tk.IntVar()
         self.__secondCheckBoxStatus = tk.IntVar()
         self.__thirdCheckBoxStatus = tk.IntVar()
+        self.__kInSize = tk.IntVar()
+        self.__kOutSize = tk.IntVar()
+        self.__beadRadiusSize = tk.IntVar()
+        self.__sphereRadiusSize = tk.IntVar()
+        self.__kbsValue = tk.IntVar()
+        self.__locationToSave = tk.StringVar()
         self.__lightMode = True
         self.__allButton = []
         self.__allEntry = []
@@ -54,15 +60,6 @@ class Gui_3D_Bio:
         self.__middleBottomFrame.pack(side='top')
 
     def grid_buttons(self):
-        rightButton = Button(self.__rightButtonsFrame, text='right button',
-                             height=1,
-                             width=15, font=("Ariel", 10, 'bold'), bg='white')
-        rightButton.pack(side='top')
-        leftButton = Button(self.__leftButtonsFrame, text='left button',
-                            height=1,
-                            width=15, font=("Ariel", 10, 'bold'), bg='white')
-        leftButton.pack(side='top')
-
         for index in range(1):
             space = Label(self.__leftButtonsFrame, text="", bg='gray92')
             space.pack(side='bottom')
@@ -72,9 +69,6 @@ class Gui_3D_Bio:
             space = Label(self.__rightButtonsFrame, text="", bg='gray92')
             space.pack(side='bottom')
             self.__allLabels.append(space)
-
-        self.__allButton.append(rightButton)
-        self.__allButton.append(leftButton)
 
     def change_mode_button(self):
         changeModeButton = Button(self.__leftButtonsFrame,
@@ -92,7 +86,7 @@ class Gui_3D_Bio:
                                      fg='black', bg='gray91',
                                      font=("Ariel", 10, 'normal'))
         self.__allLabelFrames.append(entryLabelFrame)
-        entryLabel = Label(entryLabelFrame, text='Insert Input:',
+        entryLabel = Label(entryLabelFrame, text='Enter Sequence:',
                            bg='gray91',
                            fg='black', font=("Ariel", 9, 'bold'))
         self.__allLabels.append(entryLabel)
@@ -144,6 +138,12 @@ class Gui_3D_Bio:
                                    fg='blue')
             thirdPlotLabel.pack(side='top', fill='both')
             self.__informativeLabels.append(thirdPlotLabel)
+
+        checkSizeLabel = Label(self.__middleBottomFrame,
+                               text=self.__kbsValue.get(),
+                               fg='blue')
+        checkSizeLabel.pack(side='top', fill='both')
+        self.__informativeLabels.append(checkSizeLabel)
 
     def dark_light_switch(self):
 
@@ -252,48 +252,162 @@ class Gui_3D_Bio:
             self.__lightMode = True
 
     def grid_plot_checkboxes(self):
-        firstCheckBox = Checkbutton(self.__leftButtonsFrame, text='First Plot',
-                                    variable=self.__firstCheckBoxStatus,
+        plot_label = Label(self.__rightButtonsFrame, text='Select Your Plots:',
+                           bg='gray92')
+        plot_label.pack(side='top')
+        self.__allLabels.append(plot_label)
+        firstCheckBox = Checkbutton(self.__rightButtonsFrame, text='First Plot'
+                                    , variable=self.__firstCheckBoxStatus,
                                     bg='gray92', fg='red')
         firstCheckBox.pack(side='top')
         self.__allCheckBoxes.append(firstCheckBox)
 
-        secondCheckBox = Checkbutton(self.__leftButtonsFrame,
+        secondCheckBox = Checkbutton(self.__rightButtonsFrame,
                                      text='Second Plot',
                                      variable=self.__secondCheckBoxStatus,
                                      bg='gray92', fg='red')
         secondCheckBox.pack(side='top')
         self.__allCheckBoxes.append(secondCheckBox)
 
-        thirdCheckBox = Checkbutton(self.__leftButtonsFrame, text='Third Plot',
+        thirdCheckBox = Checkbutton(self.__rightButtonsFrame,
+                                    text='Third Plot',
                                     variable=self.__thirdCheckBoxStatus,
                                     bg='gray92', fg='red')
         thirdCheckBox.pack(side='top')
         self.__allCheckBoxes.append(thirdCheckBox)
 
     def grid_scales(self):
-        k_in_label = Label(self.__middleTopFrame, text="K-In:",
+        k_in_LabelFrame = LabelFrame(self.__middleTopFrame,
+                                     text='Argument 1:',
+                                     fg='black', bg='gray91',
+                                     font=("Ariel", 10, 'normal'))
+        self.__allLabelFrames.append(k_in_LabelFrame)
+
+        k_out_LabelFrame = LabelFrame(self.__middleTopFrame,
+                                      text='Argument 2:',
+                                      fg='black', bg='gray91',
+                                      font=("Ariel", 10, 'normal'))
+        self.__allLabelFrames.append(k_out_LabelFrame)
+
+        k_in_LabelFrame.pack(side='left')
+        k_out_LabelFrame.pack(side='left')
+
+        k_in_label = Label(k_in_LabelFrame, text="K-In:",
                            bg='gray91', fg='black', font=("Ariel", 9, 'bold'))
-        k_in_scale = Scale(self.__middleTopFrame, from_=0, to=100,
-                           orient=HORIZONTAL)
-        k_in_scale.set(50)
+        k_in_scale = Entry(k_in_LabelFrame,
+                           textvariable=self.__kInSize, bd=2)
         k_in_label.pack(side='left')
         k_in_scale.pack(side='left')
         self.__allLabels.append(k_in_label)
 
-        k_out_label = Label(self.__middleTopFrame,
+        k_out_label = Label(k_out_LabelFrame,
                             text="K-Out:", bg='gray91',
                             fg='black', font=("Ariel", 9, 'bold'))
 
-        k_out_scale = Scale(self.__middleTopFrame, from_=0, to=100,
-                            orient=HORIZONTAL)
-        k_out_scale.set(50)
+        k_out_scale = Entry(k_out_LabelFrame,
+                            textvariable=self.__kOutSize, bd=2)
         k_out_label.pack(side='left')
         k_out_scale.pack(side='left')
         self.__allLabels.append(k_out_label)
 
         self.__allScales.append(k_in_scale)
         self.__allScales.append(k_out_scale)
+
+    def grid_left_side_widgets(self):
+        entryLabelFrame = LabelFrame(self.__leftButtonsFrame,
+                                     text='Output Save Location',
+                                     fg='black', bg='gray92',
+                                     font=("Ariel", 10, 'normal'))
+        self.__allLabelFrames.append(entryLabelFrame)
+        entryLabel = Label(entryLabelFrame, text='Enter Location For RMF:',
+                           bg='gray92',
+                           fg='black', font=("Ariel", 9, 'bold'))
+        self.__allLabels.append(entryLabel)
+        entryPlace = Entry(entryLabelFrame,
+                           textvariable=self.__locationToSave, bd=2)
+        self.__allEntry.append(entryPlace)
+
+        entryLabelFrame.pack(side='top', padx=7, pady=7)
+        entryLabel.pack(side='top')
+        entryPlace.pack(side='top')
+
+        for index in range(1):
+            space = Label(self.__leftButtonsFrame, text="", bg='gray92')
+            space.pack(side='top')
+            self.__allLabels.append(space)
+
+        bead_radius_LabelFrame = LabelFrame(self.__leftButtonsFrame,
+                                            text='Argument 3:',
+                                            fg='black', bg='gray92',
+                                            font=("Ariel", 10, 'normal'))
+        self.__allLabelFrames.append(bead_radius_LabelFrame)
+
+        bead_radius_LabelFrame.pack(side='top')
+
+        bead_radius_label = Label(bead_radius_LabelFrame,
+                                  text="Enter Bead Radius:", bg='gray92',
+                                  fg='black', font=("Ariel", 9, 'bold'))
+
+        bead_radius_entry = Entry(bead_radius_LabelFrame,
+                                  textvariable=self.__beadRadiusSize, bd=2)
+
+        bead_radius_label.pack()
+        bead_radius_entry.pack()
+        self.__allLabels.append(bead_radius_label)
+        self.__allEntry.append(bead_radius_entry)
+
+        for index in range(1):
+            space = Label(self.__leftButtonsFrame, text="", bg='gray92')
+            space.pack(side='top')
+            self.__allLabels.append(space)
+
+        sphere_radius_LabelFrame = LabelFrame(self.__leftButtonsFrame,
+                                              text='Argument 4:',
+                                              fg='black', bg='gray92',
+                                              font=("Ariel", 10, 'normal'))
+        self.__allLabelFrames.append(sphere_radius_LabelFrame)
+
+        sphere_radius_LabelFrame.pack(side='top')
+
+        sphere_radius_label = Label(sphere_radius_LabelFrame,
+                                    text="Enter Sphere Radius:", bg='gray92',
+                                    fg='black', font=("Ariel", 9, 'bold'))
+
+        sphere_radius_scale = Entry(sphere_radius_LabelFrame,
+                                    textvariable=self.__sphereRadiusSize, bd=2)
+        sphere_radius_label.pack()
+        sphere_radius_scale.pack()
+        self.__allLabels.append(sphere_radius_label)
+        self.__allScales.append(sphere_radius_scale)
+
+        for index in range(1):
+            space = Label(self.__leftButtonsFrame, text="", bg='gray92')
+            space.pack(side='top')
+            self.__allLabels.append(space)
+
+        kbs_LabelFrame = LabelFrame(self.__leftButtonsFrame,
+                                    text='Argument 5:',
+                                    fg='black', bg='gray92',
+                                    font=("Ariel", 10, 'normal'))
+        self.__allLabelFrames.append(kbs_LabelFrame)
+
+        kbs_LabelFrame.pack(side='top')
+
+        kbs_label = Label(kbs_LabelFrame,
+                          text="Enter KBS:", bg='gray92',
+                          fg='black', font=("Ariel", 9, 'bold'))
+
+        kbs_scale = Entry(kbs_LabelFrame,
+                          textvariable=self.__kbsValue, bd=2)
+        kbs_label.pack()
+        kbs_scale.pack()
+        self.__allLabels.append(kbs_label)
+        self.__allScales.append(kbs_scale)
+
+        for index in range(4):
+            space = Label(self.__leftButtonsFrame, text="", bg='gray92')
+            space.pack(side='top')
+            self.__allLabels.append(space)
 
 
 def main():
@@ -304,6 +418,7 @@ def main():
     program.grid_plot_checkboxes()
     program.grid_entry()
     program.grid_scales()
+    program.grid_left_side_widgets()
     program.change_mode_button()
     root.title("Hackathon Program")
     root.geometry('800x800')
