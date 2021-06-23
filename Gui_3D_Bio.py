4e5eadfbc89d948611814d4fb2ccfc4a798f6f06
Ofek Kaveh <kaofek@gmail.com>
2021-06-23 14:44:40 +0300

    added new widgets

--- Gui_3D_Bio.py
+++ Gui_3D_Bio.py
@@ -17,11 +17,17 @@ class Gui_3D_Bio:
                                          bg='gray94')
 
         self.__firstArgument = tk.StringVar()
+        self.__firstCheckBoxStatus = tk.IntVar()
+        self.__secondCheckBoxStatus = tk.IntVar()
+        self.__thirdCheckBoxStatus = tk.IntVar()
         self.__lightMode = True
         self.__allButton = []
         self.__allEntry = []
         self.__allLabels = []
         self.__allLabelFrames = []
+        self.__informativeLabels = []
+        self.__allCheckBoxes = []
+        self.__allScales = []
 
     def add_space_left_button_menu(self, n):
         """
@@ -57,14 +63,6 @@ class Gui_3D_Bio:
                             width=15, font=("Ariel", 10, 'bold'), bg='white')
         leftButton.pack(side='top')
 
-        changeModeButton = Button(self.__leftButtonsFrame,
-                                  text='Dark/Light Mode',
-                                  height=1,
-                                  width=15, font=("Ariel", 10, 'bold'),
-                                  bg='white',
-                                  command=lambda: self.dark_light_switch())
-        changeModeButton.pack(side='top')
-
         for index in range(1):
             space = Label(self.__leftButtonsFrame, text="", bg='gray92')
             space.pack(side='bottom')
@@ -77,6 +75,15 @@ class Gui_3D_Bio:
 
         self.__allButton.append(rightButton)
         self.__allButton.append(leftButton)
+
+    def change_mode_button(self):
+        changeModeButton = Button(self.__leftButtonsFrame,
+                                  text='Dark/Light Mode',
+                                  height=1,
+                                  width=15, font=("Ariel", 10, 'bold'),
+                                  bg='white',
+                                  command=lambda: self.dark_light_switch())
+        changeModeButton.pack(side='top')
         self.__allButton.append(changeModeButton)
 
     def grid_entry(self):
@@ -98,7 +105,7 @@ class Gui_3D_Bio:
                               command=lambda: self.submit_input())
         self.__allButton.append(submitButton)
 
-        entryLabelFrame.pack(padx=7, pady=7)
+        entryLabelFrame.pack(side='left', padx=7, pady=7)
         entryLabel.pack(side='top')
         entryPlace.pack(side='top')
 
@@ -110,8 +117,33 @@ class Gui_3D_Bio:
         submitButton.pack(side='top')
 
     def submit_input(self):
+        for label in self.__informativeLabels:
+            label.pack_forget()
         firstArgument = self.__firstArgument.get()
-        print(firstArgument)
+        isFirstPlot = self.__firstCheckBoxStatus.get()
+        isSecondPlot = self.__secondCheckBoxStatus.get()
+        isThirdPlot = self.__thirdCheckBoxStatus.get()
+
+        if isFirstPlot == 1:
+            firstPlotLabel = Label(self.__middleBottomFrame,
+                                   text='You Chose To Show First Plot',
+                                   fg='blue')
+            firstPlotLabel.pack(side='top', fill='both')
+            self.__informativeLabels.append(firstPlotLabel)
+
+        if isSecondPlot == 1:
+            secondPlotLabel = Label(self.__middleBottomFrame,
+                                    text='You Chose To Show Second Plot',
+                                    fg='blue')
+            secondPlotLabel.pack(side='top', fill='both')
+            self.__informativeLabels.append(secondPlotLabel)
+
+        if isThirdPlot == 1:
+            thirdPlotLabel = Label(self.__middleBottomFrame,
+                                   text='You Chose To Show Third Plot',
+                                   fg='blue')
+            thirdPlotLabel.pack(side='top', fill='both')
+            self.__informativeLabels.append(thirdPlotLabel)
 
     def dark_light_switch(self):
 
@@ -120,7 +152,6 @@ class Gui_3D_Bio:
             self.__leftButtonsFrame.configure(background='gray17')
             self.__rightButtonsFrame.configure(background='gray17')
             self.__middleTopFrame.configure(background='gray18')
-            self.__middleBottomFrame.configure(background='gray22')
             for label in self.__allLabels:
                 if label['bg'] == 'gray98':
                     label.config(bg='gray20')
@@ -131,6 +162,7 @@ class Gui_3D_Bio:
                 if label['bg'] == 'gray94':
                     label.config(bg='gray22')
                 label.config(fg='white')
+
             for entry in self.__allEntry:
                 if entry['bg'] == 'gray98':
                     entry.config(bg='gray20')
@@ -141,9 +173,11 @@ class Gui_3D_Bio:
                 if entry['bg'] == 'gray94':
                     entry.config(bg='gray22')
                 entry.config(fg='white')
+
             for button in self.__allButton:
                 button.config(bg='black')
                 button.config(fg='white')
+
             for labelFrame in self.__allLabelFrames:
                 if labelFrame['bg'] == 'gray98':
                     labelFrame.config(bg='gray20')
@@ -154,13 +188,23 @@ class Gui_3D_Bio:
                 if labelFrame['bg'] == 'gray94':
                     labelFrame.config(bg='gray22')
                 labelFrame['fg'] = 'white'
+
+            for checkBox in self.__allCheckBoxes:
+                if checkBox['bg'] == 'gray98':
+                    checkBox.config(bg='gray20')
+                if checkBox['bg'] == 'gray92':
+                    checkBox.config(bg='gray17')
+                if checkBox['bg'] == 'gray91':
+                    checkBox.config(bg='gray18')
+                if checkBox['bg'] == 'gray94':
+                    checkBox.config(bg='gray22')
+
             self.__lightMode = False
         else:
             self.__titleFrame.configure(background='gray98')
             self.__leftButtonsFrame.configure(background='gray92')
             self.__rightButtonsFrame.configure(background='gray92')
             self.__middleTopFrame.configure(background='gray91')
-            self.__middleBottomFrame.configure(background='gray94')
             for label in self.__allLabels:
                 if label['bg'] == 'gray20':
                     label.config(bg='gray98')
@@ -171,6 +215,7 @@ class Gui_3D_Bio:
                 if label['bg'] == 'gray22':
                     label.config(bg='gray94')
                 label.config(fg='black')
+
             for entry in self.__allEntry:
                 if entry['bg'] == 'gray20':
                     entry.config(bg='gray98')
@@ -194,7 +239,61 @@ class Gui_3D_Bio:
                 if labelFrame['bg'] == 'gray22':
                     labelFrame.config(bg='gray94')
                 labelFrame['fg'] = 'black'
+            for checkBox in self.__allCheckBoxes:
+                if checkBox['bg'] == 'gray20':
+                    checkBox.config(bg='gray98')
+                if checkBox['bg'] == 'gray17':
+                    checkBox.config(bg='gray92')
+                if checkBox['bg'] == 'gray18':
+                    checkBox.config(bg='gray91')
+                if checkBox['bg'] == 'gray22':
+                    checkBox.config(bg='gray94')
+
             self.__lightMode = True
+
+    def grid_plot_checkboxes(self):
+        firstCheckBox = Checkbutton(self.__leftButtonsFrame, text='First Plot',
+                                    variable=self.__firstCheckBoxStatus,
+                                    bg='gray92', fg='red')
+        firstCheckBox.pack(side='top')
+        self.__allCheckBoxes.append(firstCheckBox)
+
+        secondCheckBox = Checkbutton(self.__leftButtonsFrame,
+                                     text='Second Plot',
+                                     variable=self.__secondCheckBoxStatus,
+                                     bg='gray92', fg='red')
+        secondCheckBox.pack(side='top')
+        self.__allCheckBoxes.append(secondCheckBox)
+
+        thirdCheckBox = Checkbutton(self.__leftButtonsFrame, text='Third Plot',
+                                    variable=self.__thirdCheckBoxStatus,
+                                    bg='gray92', fg='red')
+        thirdCheckBox.pack(side='top')
+        self.__allCheckBoxes.append(thirdCheckBox)
+
+    def grid_scales(self):
+        k_in_label = Label(self.__middleTopFrame, text="K-In:",
+                           bg='gray91', fg='black', font=("Ariel", 9, 'bold'))
+        k_in_scale = Scale(self.__middleTopFrame, from_=0, to=100,
+                           orient=HORIZONTAL)
+        k_in_scale.set(50)
+        k_in_label.pack(side='left')
+        k_in_scale.pack(side='left')
+        self.__allLabels.append(k_in_label)
+
+        k_out_label = Label(self.__middleTopFrame,
+                            text="K-Out:", bg='gray91',
+                            fg='black', font=("Ariel", 9, 'bold'))
+
+        k_out_scale = Scale(self.__middleTopFrame, from_=0, to=100,
+                            orient=HORIZONTAL)
+        k_out_scale.set(50)
+        k_out_label.pack(side='left')
+        k_out_scale.pack(side='left')
+        self.__allLabels.append(k_out_label)
+
+        self.__allScales.append(k_in_scale)
+        self.__allScales.append(k_out_scale)
 
 
 def main():
@@ -202,7 +301,10 @@ def main():
     program = Gui_3D_Bio(root)
     program.root_init()
     program.grid_buttons()
+    program.grid_plot_checkboxes()
     program.grid_entry()
+    program.grid_scales()
+    program.change_mode_button()
     root.title("Hackathon Program")
     root.geometry('800x800')
     root.resizable(False, False)
